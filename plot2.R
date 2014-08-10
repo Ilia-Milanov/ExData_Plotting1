001bc91915d16bc5fafd01eb202fabdccc2cb01f
Ilia-Milanov <Ilia-Milanov@users.noreply.github.com>
2014-08-11 00:52:05 +0300

    corrections

--- plot2.R
+++ plot2.R
@@ -1,9 +1,9 @@
-fulldata <- read.csv2("household_power_consumption.txt", na.strings = "?")
+fulldata <- read.csv("household_power_consumption.txt", na.strings = "?", sep = ";")
 dates <- ((fulldata[, 1] == "1/2/2007") | (fulldata[, 1] == "2/2/2007"))
 interdata <- fulldata[dates, ]
 datetime <- strptime(paste(interdata[, 1], interdata[, 2]), "%d/%m/%Y %H:%M:%S")
 interdata[, 3] <- as.numeric(interdata[, 3])
 png(filename = "plot2.png")
-plot(datetime, interdata[, 3]/1000, type = "n", ylab = "Global Active Power (kilowatts)", xlab = "")
-lines(datetime, interdata[, 3]/1000)
+plot(datetime, interdata[, 3], type = "n", ylab = "Global Active Power (kilowatts)", xlab = "")
+lines(datetime, interdata[, 3])
 dev.off()
